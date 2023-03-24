cc5037068212057ce875c9403d4f9076eb683ad4
Diandria Xavier <diandriaxavier@gmail.com>
2023-03-24 00:03:59 -0300

    pega o arquivo ai daini

---
+++ logic.js
@@ -0,0 +1,5 @@
+function subtract_quantity () {
+    let quantity = document.querySelector("#product-quantity").value;
+    let quantity_number = parseInt(quantity);
+    let new_quantity = quantity_number - 1;
+}
