7ff093d0e93e6ae50db9f46656b8fb01cefad647
DaianedaSilva <daiane.supereira@gmail.com>
2023-03-25 12:04:45 -0300

    screen1 okay

--- logic.js
+++ logic.js
@@ -3,3 +3,13 @@ function subtract_quantity () {
     let quantity_number = parseInt(quantity);
     let new_quantity = quantity_number - 1;
 }
+
+function changeImage(img){
+    let img_src = img.src
+
+    if( img_src.includes("filled")){
+        img.src= '../../src/heart.png'
+    }else{
+        img.src= '../../src/filled-heart.png'
+    }
+}
